c55b21cccb90526bc89841191e21b84311946aeb
LUAN CONSTANCIO DO PRADO <2021104056@unibrasil.com.br>
2022-08-30 20:10:40 -0300

    Add project files.

---
+++ AulaTeste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AulaTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
