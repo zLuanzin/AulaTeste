6e91333cb429ea1bd1c7f0bbccaeed4b9bd1941b
2021104056 <2021104056@L07M29>
2022-08-30 20:18:41 -0300

    nova versão

--- AulaTeste/Program.cs
+++ AulaTeste/Program.cs
@@ -6,7 +6,7 @@ namespace AulaTeste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2");
         }
     }
 }
